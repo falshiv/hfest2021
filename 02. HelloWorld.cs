3fcb4b94c2f9472845130ba02150601fbeb3d14e
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2019-10-04 08:12:52 +0300

    hello world

---
+++ 02. HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._CSharp_Intro_and_Basic_Syntax___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
